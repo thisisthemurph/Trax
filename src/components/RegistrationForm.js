d83a44dd9e60c54bd268396ea1e9a9ffdaa06354
= <michael.w.murphy@outlook.com>
2020-04-21 16:23:21 +0100

    Updated the RegistrationForm component

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -6,37 +6,155 @@ import GenderElement from './GenderElement'
 
 class RegistrationForm extends Component {
 	state = {
-		name: '',
-		email: '',
-		sex: 'f',
-		password: '',
-		password2: '',
+		error: null,
+
 		buttonDisabled: false,
-		error: null
+		formHasErrors: false,
+		formErrors: {
+			name: '',
+			email: '',
+			password: '',
+			password2: ''
+		},
+
+		name: {
+			value: '',
+			valid: false,
+			focused: false
+		},
+		email: {
+			value: '',
+			valid: true,
+			focused: false
+		},
+		sex: {
+			value: 'f'
+		},
+		password: {
+			value: '',
+			valid: true,
+			focused: false
+		},
+		password2: {
+			value: '',
+			valid: true,
+			focused: false
+		}
 	}
 
 	setInputValue = (property, value) => {
 		this.setState({
-			[property]: value.trim()
+			[property]: {
+				...this.state[property],
+				value: value
+			}
+		})
+	}
+
+	cleanInputValue = (property) => {
+		this.setState({
+			[property]: this.state[property].trim()
+		})
+	}
+
+	validateField = (fieldName) => {
+		const value = this.state[fieldName].value
+
+		let nameValid = this.state.name.valid
+		let emailValid = this.state.email.valid
+		let passwordValid = this.state.password.valid
+		let password2Valid = this.state.password2.valid
+		let formErrors = this.state.formErrors
+
+		switch(fieldName) {
+			case 'name':
+				nameValid = value.length >= 1
+				formErrors.name = nameValid ? '' : 'You forgot to tell us what to call you'
+				break
+
+			case 'email':
+				emailValid = Boolean(value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i))
+				formErrors.email = emailValid ? '' : 'There seems to be an issue with your email address'
+				break
+			
+			case 'password':
+				passwordValid = value.length >= 6
+				formErrors.password = passwordValid ? '' : 'The password must be at least 6 characters long'
+				break
+			
+			case 'password2':
+				password2Valid = value === this.state.password.value
+				formErrors.password2 = password2Valid ? '' : 'Those passwords don\'t look the same 👀'
+				break			
+
+			default:
+				break;
+		}
+
+		this.setState({
+			formErrors,
+			name: { ...this.state.name, valid: nameValid },
+			email: { ...this.state.email, valid: emailValid },
+			password: { ...this.state.password, valid: passwordValid },
+			password2: { ...this.state.password2, valid: password2Valid }
+		}, this.validateForm)
+	}
+
+	validateForm = () => {
+		const { name, email, password, password2 } = this.state
+
+		const allElementsFocused = name.focused && email.focused && password.focused && password2.focused
+		
+		let formIsValid = false
+		if (allElementsFocused) {
+			formIsValid = name.valid && email.valid && password.valid && password2.valid
+		} else {
+			formIsValid = false;
+		}
+
+		this.setState({ 
+			formHasErrors: !formIsValid
+		})
+
+		console.log({
+			formHasErrors: this.state.formHasErrors,
+			name: this.state.name,
+			email: this.state.email,
+			password: this.state.password,
+			password2: this.state.password2
 		})
 	}
 
 	resetForm = () => {
 		this.setState({
-			name: '',
-			email: '',
-			sex: 'f',
-			password: '',
-			password2: '',
+			error: null,
+	
 			buttonDisabled: false,
-			error: null
+			formHasErrors: false,
+			formErrors: {
+				name: '',
+				email: '',
+				password: '',
+				password2: ''
+			},
+
+			password: {
+				value: '',
+				valid: true,
+				focused: false
+			},
+			password2: {
+				value: '',
+				valid: true,
+				focused: false
+			}
 		})
 	}
 
 	doRegistration = async () => {
 		const {name, email, sex, password, password2} = this.state
-		if (!name || !email || !password || !password2) {
-			console.log('One of the required input elements was blank')
+		if (!name.value || !email.value || !password.value || !password2.value) {
+			alert('Please fill out the entrire form')
 			return
 		}
 
@@ -78,7 +196,40 @@ class RegistrationForm extends Component {
 			return <p className='alert error'>{this.state.error}</p>
 		}
 
+		const allErrors = Object.values(this.state.formErrors).filter(errorMsg => errorMsg.length > 0)
+
+		if (allErrors.length > 0) {
+			return (
+				<div className='alert error'>
+					{allErrors.map((errorMsg, idx) => {
+						return <p key={idx}>{ errorMsg }</p>
+					})}
+				</div>
+			)
+		}
+
 		return null
+	}
+
+	isValid = property => {
+		const elem = this.state[property]
+
+		if (elem.valid)
+			return true
+		else
+			if (elem.focused)
+				return false
+			else
+				return true
+	}
+
+	setFocused = property => {
+		this.setState({
+			[property]: {
+				...this.state[property],
+				focused: true
+			}
+		})
 	}
 
 	render() {
@@ -90,42 +241,54 @@ class RegistrationForm extends Component {
 
 				<Input
 					type='text'
-					placeholder='Name'
-					value={this.state.name}
-					onChange={ (value) => this.setInputValue('name', value)}
+					placeholder='What is your name?'
+					value={this.state.name.value}
+					valid={this.isValid('name')}
+					onChange={ (value) => { this.setInputValue('name', value) } }
+					onFocus={ () => { this.setFocused('name') }}
+					onBlur={ () => { this.validateField('name') } }
 				/>
 
 				<Input
 					type='email'
 					placeholder='your@email.com'
-					value={this.state.email}
+					value={this.state.email.value}
+					valid={this.isValid('email')}
 					onChange={ (value) => this.setInputValue('email', value)}
+					onFocus={ () => { this.setFocused('email') }}
+					onBlur={ () => { this.validateField('email') } }
 				/>
 
 				<GenderElement
-					sex={this.state.sex}
+					sex={this.state.sex.value}
 					updateSelected={(sex) => {
-						this.setState({ sex })
+						this.setState({ 'sex': {value: sex} })
 					}}
 				/>
 
 				<Input
 					type='password'
 					placeholder='Password'
-					value={this.state.password}
+					value={this.state.password.value}
+					valid={this.isValid('password')}
 					onChange={ (value) => this.setInputValue('password', value)}
+					onFocus={ () => { this.setFocused('password') }}
+					onBlur={ () => { this.validateField('password') } }
 				/>
 
 				<Input
 					type='password'
 					placeholder='Password again please'
-					value={this.state.password2}
+					value={this.state.password2.value}
+					valid={this.isValid('password2')}
 					onChange={ (value) => this.setInputValue('password2', value)}
+					onFocus={ () => { this.setFocused('password2') }}
+					onBlur={ () => { this.validateField('password2') } }
 				/>
 
 				<Button
 					text='Sign up'
-					disabled={this.state.buttonDisabled}
+					disabled={this.buttonDisabled}
 					onClick={ () => this.doRegistration() }
 				/>
 
