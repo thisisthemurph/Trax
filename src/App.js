fc64c4f797c2b19fa0e23d4df9238b084aec973f
= <michael.w.murphy@outlook.com>
2020-05-19 15:56:17 +0100

    Moved the BrowserRouter component from the App.js component to the main index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import { ProtectedRoute } from "./components/ProtectedRoute"
 import { LoginForm, RegistrationForm } from "./components/forms"
 import { Home, Profile } from "./components/pages"
@@ -25,39 +25,33 @@ const App = () => {
 	})
 
 	return (
-		<Router>
-			<UserContext.Provider value={[user, setUser]}>
-				<div className="App">
-					<Header />
+		<UserContext.Provider value={[user, setUser]}>
+			<div className="App">
+				<Header />
 
-					<main>
-						<Switch>
-							<Route exact path="/">
-								<Home />
-							</Route>
+				<main>
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
 
-							<Route
-								exact
-								path="/login"
-								render={(props) => <LoginForm {...props} />}
-							/>
+						<Route exact path="/login" render={(props) => <LoginForm {...props} />} />
 
-							<Route exact path="/signup" component={RegistrationForm} />
+						<Route exact path="/signup" component={RegistrationForm} />
 
-							<ProtectedRoute exact path="/profile" component={Profile} />
+						<ProtectedRoute exact path="/profile" component={Profile} />
 
-							<ProtectedRoute exact path="/track/:trackId" component={TrackView} />
+						<ProtectedRoute exact path="/track/:trackId" component={TrackView} />
 
-							<Route path="*">
-								<h1>404!</h1>
-								<h2>Ah Ah Ah!</h2>
-								<h2>You didn't say the magic word!</h2>
-							</Route>
-						</Switch>
-					</main>
-				</div>
-			</UserContext.Provider>
-		</Router>
+						<Route path="*">
+							<h1>404!</h1>
+							<h2>Ah Ah Ah!</h2>
+							<h2>You didn't say the magic word!</h2>
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</UserContext.Provider>
 	)
 }
 
