8dfd966de62ff25587038a72525d5ccbdf1fde54
= <michael.w.murphy@outlook.com>
2020-05-23 08:24:37 +0100

    Improved the calculation for percentage of target reached

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,12 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
+import { FloatingActionButton } from "../form-components"
 import TrackList from "../TrackList"
+import Popup from "../Popup"
+import NewTrackForm from "../forms/NewTrackForm"
 
 const ProfilePage = () => {
+	const [showNewTrackForm, setShowNewTrackForm] = useState(false)
+	const [refreshTrackList, setRefreshTrackList] = useState(false)
+
 	return (
 		<>
 			<h1>Profile Page</h1>
-			<TrackList />
+
+			<TrackList refresh={refreshTrackList} />
+
+			<FloatingActionButton onClick={() => setShowNewTrackForm(true)}>
+				<PlusIcon />
+			</FloatingActionButton>
+
+			<Popup
+				heading="Create a new track"
+				show={showNewTrackForm}
+				onClose={() => setShowNewTrackForm(false)}
+			>
+				<NewTrackForm
+					onSuccess={() => {
+						setShowNewTrackForm(false)
+						setRefreshTrackList(!refreshTrackList)
+					}}
+				/>
+			</Popup>
 		</>
+	)
+}
+
+const PlusIcon = () => {
+	return (
+		<svg
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="white"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="feather feather-plus"
+		>
+			<line x1="12" y1="5" x2="12" y2="19"></line>
+			<line x1="5" y1="12" x2="19" y2="12"></line>
+		</svg>
 	)
 }
 
